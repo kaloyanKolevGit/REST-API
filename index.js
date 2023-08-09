1ccbc6c3558f3cec42874e5ac5ac21f48c4697db
Kaloyan Kolev <kaloyan21kolev@gmail.com>
2023-08-09 20:01:34 +0300

    Update index.js

--- index.js
+++ index.js
@@ -14,7 +14,10 @@ dbConnector()
     const app = require('express')();
     require('./config/express')(app);
 
-    app.use(cors());
+    app.use(cors({
+      origin: config.origin,
+      credentials: true
+    }));
 
     app.use('/api', apiRouter);
 
