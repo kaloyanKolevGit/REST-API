ac4069ade31d42b51ca913668e7f56369c17662c
Kaloyan Kolev <kaloyan21kolev@gmail.com>
2023-08-09 21:30:31 +0300

    Update express.js

--- config/express.js
+++ config/express.js
@@ -13,10 +13,7 @@ module.exports = (app) => {
 
     app.use(express.static(path.resolve(__basedir, 'static')));
 
-    app.use(cors({
-        origin: config.origin,
-        credentials: true
-      }));
+    app.use(cors());
 
 
     // app.use(errorHandler(err, req, res, next));
