124d802231c93b82e0219ae1fba64f969281dcfa
Kaloyan Kolev <kaloyan21kolev@gmail.com>
2023-08-09 21:14:02 +0300

    Update express.js

--- config/express.js
+++ config/express.js
@@ -17,13 +17,7 @@ module.exports = (app) => {
         origin: config.origin,
         credentials: true
       }));
-      
-
-    app.use(function(req, res, next) {
-        res.header("Access-Control-Allow-Origin", "*");
-        res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-        next();
-    });
+    
 
     // app.use(errorHandler(err, req, res, next));
 };
