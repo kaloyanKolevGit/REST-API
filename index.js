405864cbf93d08de0b4ad2d7869daa4554025c64
Kaloyan Kolev <kaloyan21kolev@gmail.com>
2023-08-09 21:10:47 +0300

    Update index.js

--- index.js
+++ index.js
@@ -13,11 +13,21 @@ dbConnector()
     const app = require('express')();
     require('./config/express')(app);
 
-    app.use(function(req, res, next) {
-      res.header("Access-Control-Allow-Origin", "*");
-      res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-      next();
-    });
+    app.use((req, res, next) => {
+      res.header("Access-Control-Allow-Origin", "*")
+      res.header(
+        "Access-Control-Allow-Headers",
+        "Origin, X-Requested, Content-Type, Accept Authorization"
+      )
+      if (req.method === "OPTIONS") {
+        res.header(
+          "Access-Control-Allow-Methods",
+          "POST, PUT, PATCH, GET, DELETE"
+        )
+        return res.status(200).json({})
+      }
+      next()
+    })
 
     app.use('/api', apiRouter);
 
